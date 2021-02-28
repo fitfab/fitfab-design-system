50e50ae0ae58e6f010e3c5d098aa79745e9cf9f2
Miguel Julio <fitfab@yahoo.com>
2021-02-28 10:07:07 -0500

    add global styles

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'fitfab-design-system',
+  globalStyle: 'src/global/styles.css',
   outputTargets: [
     {
       type: 'dist',
